6fa3c25d71775898ca3f02c52835caf40de1c115
awsbasiya <awsbasiya@localhost>
2023-04-06 16:32:19 -0400

    Test commit

---
+++ src/Demo/Test4.java
@@ -0,0 +1,20 @@
+package Demo;
+
+public class Test4 {
+	
+	
+	
+	
+public static void main (String [] args) {
+		
+		System.out.println("Test........");
+		
+		System.out.println("Git AWS......");
+		
+		
+	}
+
+}
+ 
+
+
